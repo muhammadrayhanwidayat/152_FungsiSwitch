ae0ba22900f288aecce14c8d6658989d2b30b34d
muhammadrayhanwidayat <murayat99@gmail.com>
2024-03-26 11:09:53 +0700

    membuat pengambilan keputusan switch

--- switch/switch.cpp
+++ switch/switch.cpp
@@ -27,54 +27,46 @@ void inputData() {
 }
 
 int main() {
-	int pilihan;
-	do {
-		system("CLS");
-		cout << "menu kalkulator sederhana" << endl;
-		cout << "1.penjumlahan" << endl;
-		cout << "2.pengurangan" << endl;
-		cout << "3.perkalian" << endl;
-		cout << "4.pembagian" << endl;
-		cout << "5.keluar" << endl;
-		cout << "masukan pilihan" << endl;
-		cin >> pilihan;
+    int pilihan;
+    do {
+        system("CLS");
+        cout << "menu kalkulator sederhana" << endl;
+        cout << "1.penjumlahan" << endl;
+        cout << "2.pengurangan" << endl;
+        cout << "3.perkalian" << endl;
+        cout << "4.pembagian" << endl;
+        cout << "5.keluar" << endl;
+        cout << "masukan pilihan: ";
+        cin >> pilihan;
 
-	} while(pilihan!=5);
+        switch (pilihan) {
+        case 1:
+            inputData();
+            cout << "\nhasil penjumlahan:" << penjumlahan(bilangan1, bilangan2) << endl;
+            system("pause");
+            break;
+        case 2:
+            inputData();
+            cout << "\nhasil pengurangan:" << pengurangan(bilangan1, bilangan2) << endl;
+            system("pause");
+            break;
+        case 3:
+            inputData();
+            cout << "\nhasil perkalian:" << perkalian(bilangan1, bilangan2) << endl;
+            system("pause");
+            break;
+        case 4:
+            inputData();
+            cout << "\nhasil pembagian:" << pembagian(bilangan1, bilangan2) << endl;
+            system("pause");
+            break;
+        case 5:
+            break;
+        default:
+            cout << "pilihan tidak valid" << endl;
+            system("pause");
+            break;
+        }
+    } while (pilihan != 5);
+    return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-int main()
-{
-
-
-}
-
