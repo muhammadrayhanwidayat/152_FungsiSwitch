dd91100dec692eea330247848fa18ddcab234109
muhammadrayhanwidayat <murayat99@gmail.com>
2024-03-26 10:47:23 +0700

    mebambahkan loopping do while dalam integer main

--- switch/switch.cpp
+++ switch/switch.cpp
@@ -26,6 +26,22 @@ void inputData() {
 	cin >> bilangan2;
 }
 
+int main() {
+	int pilihan;
+	do {
+		system("CLS");
+		cout << "menu kalkulator sederhana" << endl;
+		cout << "1.penjumlahan" << endl;
+		cout << "2.pengurangan" << endl;
+		cout << "3.perkalian" << endl;
+		cout << "4.pembagian" << endl;
+		cout << "5.keluar" << endl;
+		cout << "masukan pilihan" << endl;
+		cin >> pilihan;
+
+	} while(pilihan!=5);
+}
+
 
 
 
